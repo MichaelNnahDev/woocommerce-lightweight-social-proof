b69de28c42bb163854de4cc62c77f967d807302f
Michael Nnah <nnahmichael@gmail.com>
2026-09-07 04:56:31 -0700

    feat: make hex text fields directly editable with bidirectional color swatch sync

--- includes/class-social-proof-admin.php
+++ includes/class-social-proof-admin.php
@@ -194,13 +194,17 @@ class WCLSP_Social_Proof_Admin {
 
     public static function field_color( $args ) {
         $opts    = self::get_options();
-        $id      = $args['id'];
+        $id      = esc_attr( $args['id'] );
         $val     = esc_attr( $opts[ $id ] );
         $tooltip = $args['tooltip'] ?? '';
         $desc    = $args['desc'] ?? '';
 
-        echo '<input type="color" name="wclsp_settings[' . esc_attr( $id ) . ']" value="' . $val . '" style="vertical-align:middle; width:46px; height:34px; padding:0; cursor:pointer;"> ';
-        echo '<input type="text" value="' . $val . '" style="width:85px; vertical-align:middle; text-transform:uppercase;" readonly>';
+        // Color picker element (swatch)
+        echo '<input type="color" id="' . $id . '_picker" value="' . $val . '" style="vertical-align:middle; width:46px; height:34px; padding:0; cursor:pointer;" oninput="document.getElementById(\'' . $id . '\').value = this.value.toUpperCase();"> ';
+
+        // Editable text input for direct typing or pasting HEX codes
+        echo '<input type="text" id="' . $id . '" name="wclsp_settings[' . $id . ']" value="' . $val . '" maxlength="7" placeholder="#000000" style="width:95px; vertical-align:middle; text-transform:uppercase; font-family:monospace; font-weight:600;" oninput="if (/^#[0-9A-Fa-f]{6}$/.test(this.value)) { document.getElementById(\'' . $id . '_picker\').value = this.value; }">';
+
         self::render_tooltip( $tooltip );
         if ( ! empty( $desc ) ) {
             echo '<p class="description" style="margin-top:4px; font-size:12px; color:#646970;">' . esc_html( $desc ) . '</p>';
